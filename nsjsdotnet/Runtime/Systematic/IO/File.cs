342bd256bbc1d2895bec9e4187f26d1e284eb934
liulilittle <1228980386@qq.com>
2018-06-20 12:15:31 +0800

    extended for javascript file support

--- nsjsdotnet/Runtime/Systematic/IO/File.cs
+++ nsjsdotnet/Runtime/Systematic/IO/File.cs
@@ -3,6 +3,8 @@
     using global::System;
     using global::System.Text;
     using nsjsdotnet.Core;
+    using nsjsdotnet.Core.IO;
+    using System.IO;
     using FILE = global::System.IO.File;
     using NSJSEncoding = nsjsdotnet.Runtime.Systematic.Text.Encoding;
 
@@ -27,8 +29,160 @@
             owner.AddFunction("WriteAllBytes", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(WriteAllBytes));
             owner.AddFunction("WriteAllText", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(WriteAllText));
 
+            owner.AddFunction("GetFileLength", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(GetFileLength));
             owner.AddFunction("ReadAllText", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(ReadAllText));
             owner.AddFunction("ReadAllBytes", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(ReadAllBytes));
+
+            owner.AddFunction("GetCreationTime", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(GetCreationTime));
+            owner.AddFunction("GetCreationTimeUtc", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(GetCreationTimeUtc));
+            owner.AddFunction("GetLastAccessTime", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(GetLastAccessTime));
+            owner.AddFunction("GetLastAccessTimeUtc", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(GetLastAccessTimeUtc));
+            owner.AddFunction("GetLastWriteTime", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(GetLastWriteTime));
+            owner.AddFunction("GetLastWriteTimeUtc", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(GetLastWriteTimeUtc));
+            owner.AddFunction("GetAttributes", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(GetAttributes));
+
+            owner.AddFunction("SetCreationTime", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(SetCreationTime));
+            owner.AddFunction("SetCreationTimeUtc", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(SetCreationTimeUtc));
+            owner.AddFunction("SetLastAccessTime", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(SetLastAccessTime));
+            owner.AddFunction("SetLastAccessTimeUtc", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(SetLastAccessTimeUtc));
+            owner.AddFunction("SetLastWriteTime", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(SetLastWriteTime));
+            owner.AddFunction("SetLastWriteTimeUtc", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(SetLastWriteTimeUtc));
+            owner.AddFunction("SetAttributes", NSJSPinnedCollection.Pinned<NSJSFunctionCallback>(SetAttributes));
+        }
+
+        private static void SetCreationTime(IntPtr info)
+        {
+            CallSetFileInfo(info, (arguments, path, datetime) => FILE.SetCreationTime(path, datetime));
+        }
+
+        private static void SetCreationTimeUtc(IntPtr info)
+        {
+            CallSetFileInfo(info, (arguments, path, datetime) => FILE.SetCreationTimeUtc(path, datetime));
+        }
+
+        private static void SetLastAccessTime(IntPtr info)
+        {
+            CallSetFileInfo(info, (arguments, path, datetime) => FILE.SetLastAccessTime(path, datetime));
+        }
+
+        private static void SetLastAccessTimeUtc(IntPtr info)
+        {
+            CallSetFileInfo(info, (arguments, path, datetime) => FILE.SetLastAccessTimeUtc(path, datetime));
+        }
+
+        private static void SetLastWriteTime(IntPtr info)
+        {
+            CallSetFileInfo(info, (arguments, path, datetime) => FILE.SetLastWriteTime(path, datetime));
+        }
+
+        private static void SetLastWriteTimeUtc(IntPtr info)
+        {
+            CallSetFileInfo(info, (arguments, path, datetime) => FILE.SetLastWriteTimeUtc(path, datetime));
+        }
+
+        private static void SetAttributes(IntPtr info)
+        {
+            CallSetFileInfo(info, (arguments, path, value) => FILE.SetAttributes(path, unchecked((FileAttributes)value)));
+        }
+
+        private static void GetCreationTime(IntPtr info)
+        {
+            CallGetFileInfo(info, (arguments, path) => arguments.SetReturnValue(FILE.GetCreationTime(path)));
+        }
+
+        private static void GetCreationTimeUtc(IntPtr info)
+        {
+            CallGetFileInfo(info, (arguments, path) => arguments.SetReturnValue(FILE.GetCreationTimeUtc(path)));
+        }
+
+        private static void GetLastAccessTime(IntPtr info)
+        {
+            CallGetFileInfo(info, (arguments, path) => arguments.SetReturnValue(FILE.GetLastAccessTime(path)));
+        }
+
+        private static void GetLastAccessTimeUtc(IntPtr info)
+        {
+            CallGetFileInfo(info, (arguments, path) => arguments.SetReturnValue(FILE.GetLastAccessTimeUtc(path)));
+        }
+
+        private static void GetLastWriteTime(IntPtr info)
+        {
+            CallGetFileInfo(info, (arguments, path) => arguments.SetReturnValue(FILE.GetLastWriteTime(path)));
+        }
+
+        private static void GetLastWriteTimeUtc(IntPtr info)
+        {
+            CallGetFileInfo(info, (arguments, path) => arguments.SetReturnValue(FILE.GetLastWriteTimeUtc(path)));
+        }
+
+        private static void GetAttributes(IntPtr info)
+        {
+            CallGetFileInfo(info, (arguments, path) => arguments.SetReturnValue(unchecked((int)FILE.GetAttributes(path))));
+        }
+
+        private static void CallSetFileInfo(IntPtr info, Action<NSJSFunctionCallbackInfo, string, int> callback)
+        {
+            if (callback == null)
+            {
+                throw new ArgumentNullException("callback");
+            }
+            CallGetFileInfo(info, (arguments, path) =>
+            {
+                NSJSInt32 n = arguments.Length > 0 ? arguments[0] as NSJSInt32 : null;
+                if (n == null)
+                {
+                    Throwable.ArgumentNullException(arguments.VirtualMachine);
+                }
+                else
+                {
+                    callback(arguments, path, n.Value);
+                }
+            });
+        }
+
+        private static void CallSetFileInfo(IntPtr info, Action<NSJSFunctionCallbackInfo, string, DateTime> callback)
+        {
+            if (callback == null)
+            {
+                throw new ArgumentNullException("callback");
+            }
+            CallGetFileInfo(info, (arguments, path) =>
+            {
+                NSJSDateTime datetime = arguments.Length > 0 ? arguments[0] as NSJSDateTime : null;
+                if (datetime == null)
+                {
+                    Throwable.ArgumentNullException(arguments.VirtualMachine);
+                }
+                else
+                {
+                    callback(arguments, path, datetime.Value);
+                }
+            });
+        }
+
+        private static void CallGetFileInfo(IntPtr info, Action<NSJSFunctionCallbackInfo, string> callback)
+        {
+            if (callback == null)
+            {
+                throw new ArgumentNullException("callback");
+            }
+            NSJSFunctionCallbackInfo arguments = NSJSFunctionCallbackInfo.From(info);
+            string path = arguments.Length > 0 ? (arguments[0] as NSJSString)?.Value : null;
+            if (!FILE.Exists(path))
+            {
+                Throwable.FileNotFoundException(arguments.VirtualMachine);
+            }
+            else
+            {
+                callback(arguments, path);
+            }
+        }
+
+        private static void GetFileLength(IntPtr info)
+        {
+            NSJSFunctionCallbackInfo arguments = NSJSFunctionCallbackInfo.From(info);
+            string path = arguments.Length > 0 ? (arguments[0] as NSJSString)?.Value : null;
+            arguments.SetReturnValue(FileAuxiliary.GetFileLength64(path));
         }
 
         private static void ReadAllBytes(IntPtr info)
